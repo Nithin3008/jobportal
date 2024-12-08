0b051f01fc630838e35a41ad292d01ca2bb7d6d7
Nithin <nithindevathi@gmail.com>
2024-12-08 16:12:21 +0530

    feat: users can their applications

--- src/pages/job.jsx
+++ src/pages/job.jsx
@@ -1,5 +1,27 @@
+import CreateApplication from "@/components/createApplication";
+import CreatedJobs from "@/components/createdJobs";
+import { useUser } from "@clerk/clerk-react";
+import { BarLoader } from "react-spinners";
+
 const Job = () => {
-  return <div>Hi Job</div>;
+  const { user, isLoaded } = useUser();
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+  return (
+    <div>
+      <h1 className="gradient-title front-extrabold text-6xl sm:text-7xl text-center pb-8">
+        {user?.unsafeMetadata?.role === "candidate"
+          ? "My Applications"
+          : "My Jobs"}
+      </h1>
+      {user?.unsafeMetadata?.role === "candidate" ? (
+        <CreateApplication />
+      ) : (
+        <CreatedJobs />
+      )}
+    </div>
+  );
 };
 
 export default Job;
